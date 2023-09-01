481c4322634da32c2e14ab74c78c9afc6d03dadb
Ziyad <142808083+Ar1sk@users.noreply.github.com>
2023-09-02 00:03:14 +0700

    Update PlayerMovement

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -5,27 +5,91 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     //Component
-    public Rigidbody2D rb;
+    private Rigidbody2D rb;
+    private Animator anim;
+
 
     //Declare
     float dirX;
+    private bool facingRight = true;
+    private bool isGrounded;
 
     //SerializeField
     [SerializeField] private float moveSpeed = 5f;
     [SerializeField] private float jumpForce = 8f;
     [SerializeField] private Transform groundCheck;
     [SerializeField] private LayerMask groundLayer;
+    [SerializeField] private float groundCheckRadius;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        if(Input.GetKeyDown(KeyCode.Space))
+            JumpButton();
+
+        CollisionCheck();
+        CheckInput();
+        FlipController();
+        AnimatorController();
+    }
+
+    private void FixedUpdate()
+    {
+    
+    }
+
+    private void CheckInput()
+    {
         dirX = Input.GetAxisRaw("Horizontal");
         rb.velocity = new Vector2(dirX * moveSpeed, rb.velocity.y);
+    }
+
+    private void JumpButton()
+    {
+        if(isGrounded)
+            Jump();
+    }
+    private void Jump()
+    {
+        rb.velocity = new Vector2(rb.velocity.x, jumpForce);
+    }
+    private void AnimatorController()
+    {
+        bool isRunning = rb.velocity.x != 0;
+
+        anim.SetFloat("yVelocity", rb.velocity.y);
+        anim.SetBool("isGrounded", isGrounded);
+        anim.SetBool("isRunning", isRunning);
+    }
+    
+    private void FlipController()
+    {
+        if (rb.velocity.x > 0 && !facingRight)
+            Flip();
+        else if (rb.velocity.x < 0 && facingRight)
+            Flip();
+    }
+
+    private void Flip()
+    {
+        facingRight = !facingRight;
+        transform.Rotate(0, 180, 0);
+    }
+
+    private void CollisionCheck()
+    {
+        isGrounded = Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, groundLayer);
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.DrawWireSphere(groundCheck.position, groundCheckRadius); 
     }
 }
